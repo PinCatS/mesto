e16c3e385acd2e2bd7ee5498291823c7fd8e0624
Sergey Li <pincats@gmail.com>
2020-11-21 02:34:56 +0300

    feat: Add profile edit popup functionality

---
+++ script.js
@@ -0,0 +1,39 @@
+function openPopup(evt) {
+  if (evt.target === profileInfoEditButton) {
+    popupNode.classList.add('popup_opened');
+
+    const profileNameNode = this.querySelector('.profile__name');
+    const profileActivityNode = this.querySelector('.profile__activity');
+    const [profileNameInput, profileActivityInput] = popupNode.querySelectorAll('.form-input');
+
+    profileNameInput.value = profileNameNode.textContent;
+    profileActivityInput.value = profileActivityNode.textContent;
+  }
+}
+
+function savePopupInput(evt) {
+  const [profileNameInput, profileActivityInput] = popupNode.querySelectorAll('.form-input');
+  const profileNameNode = profileInfoTextNode.querySelector('.profile__name');
+  const profileActivityNode = profileInfoTextNode.querySelector('.profile__activity');
+
+  profileNameNode.textContent = profileNameInput.value;
+  profileActivityNode.textContent = profileActivityInput.value;
+
+  closePopup(evt);
+}
+
+function closePopup(evt) {
+  popupNode.classList.remove('popup_opened');
+  evt.preventDefault();
+}
+
+const popupNode = document.querySelector('.popup');
+const popupSaveButton = popupNode.querySelector('.popup__button');
+const popupCloseButton = popupNode.querySelector('.popup__close-button');
+
+const profileInfoTextNode = document.querySelector('.profile__info-text');
+const profileInfoEditButton = profileInfoTextNode.querySelector('.profile__edit-button');
+
+profileInfoTextNode.addEventListener('click', openPopup);
+popupSaveButton.addEventListener('click', savePopupInput);
+popupCloseButton.addEventListener('click', closePopup);
