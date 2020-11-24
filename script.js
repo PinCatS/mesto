e695d7b5394559f4150e5224f4ce97b3d8e8db4b
Sergey Li <pincats@gmail.com>
2020-11-24 10:55:50 +0300

    refactor: Move function definitions to the end of file

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/* Getting form, profile info and buttons */
+let popupNode = document.querySelector('.popup');
+let popupForm = popupNode.querySelector('.popup__container');
+let profileInfoTextNode = document.querySelector('.profile__info-text');
+
+let popupCloseButton = popupNode.querySelector('.popup__close-button');
+let profileInfoEditButton = profileInfoTextNode.querySelector('.profile__edit-button');
+
+/* Getting nodes with profile name and activity */
+let profileNameNode = profileInfoTextNode.querySelector('.profile__name');
+let profileActivityNode = profileInfoTextNode.querySelector('.profile__activity');
+
+/* Getting form name and activity inputs */
+let profileNameInput = popupNode.querySelector('.popup__input_name_name');
+let profileActivityInput = popupNode.querySelector('.popup__input_name_activity');
+
+/* Add event listeners */
+popupForm.addEventListener('submit', savePopupInput);
+popupCloseButton.addEventListener('click', closePopup);
+profileInfoEditButton.addEventListener('click', openPopup);
+
+
 /* Handlers definition and helper functions */
 function populateInputWithCurrentValues() {
   profileNameInput.value = profileNameNode.textContent;
@@ -21,25 +43,4 @@ function savePopupInput(evt) {
 
   closePopup();
 }
-
-/* Getting form, profile info and buttons */
-let popupNode = document.querySelector('.popup');
-let popupForm = popupNode.querySelector('.popup__container');
-let profileInfoTextNode = document.querySelector('.profile__info-text');
-
-let popupCloseButton = popupNode.querySelector('.popup__close-button');
-let profileInfoEditButton = profileInfoTextNode.querySelector('.profile__edit-button');
-
-/* Getting nodes with profile name and activity */
-let profileNameNode = profileInfoTextNode.querySelector('.profile__name');
-let profileActivityNode = profileInfoTextNode.querySelector('.profile__activity');
-
-/* Getting form name and activity inputs */
-let profileNameInput = popupNode.querySelector('.popup__input_name_name');
-let profileActivityInput = popupNode.querySelector('.popup__input_name_activity');
-
-/* Add event listeners */
-popupForm.addEventListener('submit', savePopupInput);
-popupCloseButton.addEventListener('click', closePopup);
-profileInfoEditButton.addEventListener('click', openPopup);
 
