20d3fe4800d9c70e27165459c1ecb3fd39596ce0
Sergey Li <pincats@gmail.com>
2020-12-04 21:52:13 +0300

    fix: Markup popups in html instead of building in js

--- script.js
+++ script.js
@@ -25,65 +25,15 @@ const initialCards = [
   }
 ];
 
-const addCardPopupElementData = {
-  popupName: 'popup_name_add-card',
-  form: {
-    name: 'add-card-form',
-    title: 'Новое место',
-    info: {
-      inputs: [
-        {
-          name: "place-name",
-          placeholder: "Название",
-          classList: ['popup__input_name_place-name'],
-        },
-        {
-          name: "place-image-url",
-          placeholder: "Ссылка на картинку",
-          classList: ['popup__input_name_place-image-url'],
-        },
-      ],
-      submitButton: {
-        text: "Создать",
-      }
-    }
-  }
-}
-
-const profileEditPopupElementData = {
-  popupName: 'popup_name_edit-profile',
-  form: {
-    name: 'profile-edit-form',
-    title: 'Редактировать профиль',
-    info: {
-      inputs: [
-        {
-          name: "profile-name",
-          placeholder: "Имя профиля",
-          classList: ['popup__input_name_name'],
-        },
-        {
-          name: "profile-activity",
-          placeholder: "Деятельность",
-          classList: ['popup__input_name_activity'],
-        },
-      ],
-      submitButton: {
-        text: "Сохранить",
-      }
-    }
-  }
-}
-
 /* Getting form, profile info and buttons */
-const popupNode = buildPopup(profileEditPopupElementData);
+const popupNode = document.querySelector('.popup_name_edit-profile');
 const popupForm = popupNode.querySelector('.popup__container');
 const profileInfoTextNode = document.querySelector('.profile__info-text');
 
 const popupCloseButton = popupForm.querySelector('.popup__close-button');
 const profileInfoEditButton = profileInfoTextNode.querySelector('.profile__edit-button');
 
-const popupAddCard = buildPopup(addCardPopupElementData);
+const popupAddCard = document.querySelector('.popup_name_add-card');
 const addCardForm = popupAddCard.querySelector('.popup__container');
 const addCardCloseButton = addCardForm.querySelector('.popup__close-button');
 const addCardButton = document.querySelector('.profile__add-button');
@@ -146,34 +96,6 @@ function savePopupInput(evt) {
   profileActivityNode.textContent = profileActivityInput.value;
 
   closePopup(evt);
-}
-
-function buildPopup(popupElementData) {
-  const popupTemplate = document.querySelector('#popup').content;
-
-  const popupCloneContent = popupTemplate.cloneNode(true);
-  const popupNode = popupCloneContent.querySelector('.popup');
-  popupNode.classList.add(popupElementData.popupName);
-
-  const formNode = popupNode.querySelector('.popup__container');
-  formNode.name = popupElementData.form.name;
-
-  formNode.querySelector('.popup__heading').textContent = popupElementData.form.title;
-
-  const formInputNodes = formNode.querySelectorAll('.popup__input');
-  formInputNodes.forEach((inputNode, i) => {
-    inputNode.name = popupElementData.form.info.inputs[i].name;
-    inputNode.placeholder = popupElementData.form.info.inputs[i].placeholder;
-    for (let cls of popupElementData.form.info.inputs[i].classList) {
-      inputNode.classList.add(cls);
-    }
-  });
-
-  formNode.querySelector('.popup__save-button').textContent = popupElementData.form.info.submitButton.text;
-
-  document.querySelector('.page').append(popupNode);
-
-  return popupNode;
 }
 
 function addNewCard(evt) {
